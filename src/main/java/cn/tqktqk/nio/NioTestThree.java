bc3796233692079a6baf2cc731ebeb74b1db5114
tqk <1282995880@qq.com>
2019-10-10 18:11:32 +0800

    完全理解buffer～

---
+++ src/main/java/cn/tqktqk/nio/NioTestThree.java
@@ -0,0 +1,43 @@
+package cn.tqktqk.nio;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * █████▒█      ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ * ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ * ░     ░░▒░ ░ ░   ░  ▒   ░ ░▒ ▒░
+ * ░ ░    ░░░ ░ ░ ░        ░ ░░ ░
+ * ░     ░ ░      ░  ░
+ *
+ * @author ：涂齐康
+ * @date ：Created in 2019/9/30 7:21 下午
+ * @description：
+ * @modified By：
+ * @version:
+ */
+public class NioTestThree {
+
+    public static void main(String[] args) throws Exception {
+        FileOutputStream fis = new FileOutputStream("TestNio3");
+        FileChannel fileChannel = fis.getChannel();
+
+        ByteBuffer byteBuffer = ByteBuffer.allocate(512);
+        byte[] bytes = "Hello world".getBytes();
+        System.out.println(bytes.length);
+        for (int i = 0; i < bytes.length; i++) {
+            byteBuffer.put(bytes[i]);
+        }
+
+        byteBuffer.flip();
+
+        fileChannel.write(byteBuffer);
+        fis.close();
+    }
+}
