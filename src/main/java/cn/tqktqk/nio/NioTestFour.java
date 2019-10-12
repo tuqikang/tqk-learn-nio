4ea0ba82ee5d8aeb679af89220657a3d7beb5767
tqk <1282995880@qq.com>
2019-10-12 12:54:20 +0800

    nio 文件复制

---
+++ src/main/java/cn/tqktqk/nio/NioTestFour.java
@@ -0,0 +1,38 @@
+package cn.tqktqk.nio;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * █████▒█      ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ * ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ * ░     ░░▒░ ░ ░   ░  ▒   ░ ░▒ ▒░
+ * ░ ░    ░░░ ░ ░ ░        ░ ░░ ░
+ * ░     ░ ░      ░  ░
+ *
+ * @author ：涂齐康
+ * @date ：Created in 2019/10/12 12:48 下午
+ * @description：
+ * @modified By：
+ * @version:
+ */
+public class NioTestFour {
+    public static void main(String[] args) throws Exception{
+        FileOutputStream fos = new FileOutputStream("OutTest");
+        FileInputStream fis = new FileInputStream("InTest");
+
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        FileChannel inChannel = fis.getChannel();
+        inChannel.read(byteBuffer);
+        FileChannel outChannel = fos.getChannel();
+        byteBuffer.flip();
+        outChannel.write(byteBuffer);
+
+    }
+}
