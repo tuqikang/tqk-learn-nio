ab56fd07647b2e9f0e3e5f010d775aa44a1b9fab
tqk <1282995880@qq.com>
2019-10-18 13:21:32 +0800

    关于字符的编解码

---
+++ src/main/java/cn/tqktqk/nio/codec/NioCodecTest.java
@@ -0,0 +1,73 @@
+package cn.tqktqk.nio.codec;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CharsetEncoder;
+
+/**
+ * █████▒█      ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ * ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ * ░     ░░▒░ ░ ░   ░  ▒   ░ ░▒ ▒░
+ * ░ ░    ░░░ ░ ░ ░        ░ ░░ ░
+ * ░     ░ ░      ░  ░
+ *
+ * @author ：涂齐康
+ * @date ：Created in 2019/10/18 12:19 下午
+ * @description：
+ * @modified By：
+ * @version:
+ */
+public class NioCodecTest {
+
+    public static void main(String[] args) throws Exception {
+        String inputFile = "NioCodecIn";
+        String outputFile = "NioCodecOut";
+
+        RandomAccessFile inputRandomAccessFile = new RandomAccessFile(inputFile, "r");
+        RandomAccessFile outputRandomAccessFile = new RandomAccessFile(outputFile, "rw");
+
+        long inputLength = new File(inputFile).length();
+
+        FileChannel inputFileChannel = inputRandomAccessFile.getChannel();
+        FileChannel outputFileChannel = outputRandomAccessFile.getChannel();
+
+        MappedByteBuffer inputData = inputFileChannel.map(FileChannel.MapMode.READ_ONLY, 0, inputLength);
+
+        System.out.println("-------------");
+
+        Charset.availableCharsets().entrySet().stream().map(p->p.getKey()+":"+p.getValue()).forEach(System.out::println);
+        System.out.println("-------------");
+
+        Charset charset = Charset.forName("utf-8");
+        //字节数组->字符串  解码器
+        CharsetDecoder charsetDecoder = charset.newDecoder();
+        //字符串->字节数组 编码器
+        CharsetEncoder charsetEncoder = charset.newEncoder();
+
+        //字符缓冲
+        CharBuffer charBuffer = charsetDecoder.decode(inputData);
+
+        ByteBuffer outputData = charsetEncoder.encode(charBuffer);
+
+        outputFileChannel.write(outputData);
+
+        inputFileChannel.close();
+        outputFileChannel.close();
+
+
+    }
+
+
+}
